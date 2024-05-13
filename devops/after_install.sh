829394e1ed2bc56a34cb09ea33ba290c0a1929d0
Thiago Arndt <thiagoarndt04@gmail.com>
2024-05-13 14:10:45 -0300

    try now

--- devops/after_install.sh
+++ devops/after_install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+
 npm install
 pm2 start npm --name nextjs-apps -- run start -- -p 3000
 # Make after_install.sh executable
