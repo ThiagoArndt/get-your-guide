44c097eea8e63ad8a9370ec68535669a755c8fdf
thiagoarndt <thiagoarndt04@gmail.com>
2024-05-14 20:17:52 -0300

    test

--- devops/after_install.sh
+++ devops/after_install.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-
+# Load NVM and Node.js environment
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
+# Change to the application directory
 cd /var/www/html
-fuser -k 3000/tcp
-npm run start
 
+# Start the application
+npm run start &
 
-              
+# Wait until the application is running on port 3000
+echo "Waiting for the application to start..."
+while ! nc -z localhost 3000; do
+    sleep 1
+done
+
+echo "Application started successfully on port 3000."
+
+# Optionally, you can add additional commands here to perform after the application starts.
+# For example:
+# echo "Performing additional tasks..."
+# <your_additional_commands_here>
+
+# Exit the script
+exit 0
