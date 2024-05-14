10105a68732db3a2d045a72c9010fd6558cda0f0
Thiago Arndt <thiagoarndt04@gmail.com>
2024-05-14 18:23:40 -0300

    test

--- devops/after_install.sh
+++ devops/after_install.sh
@@ -6,7 +6,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 cd /var/www/html
-npm install
 npm install pm2 -g
 pm2 delete all
 pm2 start npm --name nextjs-apps -- run start -- -p 3000
